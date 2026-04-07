88ce818718dacb5f33049a406ca82ccf2dd1b5d6
mohamedmarkhi <markhimohamed@outlook.fr>
2026-04-07 17:02:32 +0100

    db architechture updated

---
+++ architecturedb.js
@@ -0,0 +1,87 @@
+// users
+// id
+// name
+// mail
+// password
+
+
+// books
+// id
+// title
+// author
+// category
+// isRented (automatically $false)
+
+
+
+// rentedBooks
+// bookId
+// userId
+// rentDate 
+// returnDate
+
+
+
+
+// (a trigger to be done: if a book is emprunted, it will be marked as emprunted)
+// (a trigger to be done: if a book is returned, it will be marked as returned)
+
+
+
+
+//To create users collection:
+
+db.createCollection("users", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["name", "mail", "password"],
+      properties: {
+        name: { bsonType: "string" },
+        mail: { bsonType: "string" },
+        password: { bsonType: "string" }
+      }
+    }
+  }
+})
+
+//To create books collection:
+
+db.createCollection("books", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object", required: ["title", "author", "category"], properties: {
+        title: { bsonType: "string", description: "Must be a string and is required" }, author: { bsonType: "string", description: "Must be a string and iw required" }, category: { bsonType: "string", description: "Must be a string and iw required" },
+        isRented: { bsonType: "bool", description: "Must be boolean", default: 0 }
+      }
+    }
+  }
+})
+
+//To create rentedBooks:
+db.createCollection("rentedBooks", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["bookId", "userId", "rentDate", "returnDate"],
+      properties: {
+        bookId: {
+          bsonType: "objectId",
+          description: "must be a valid book ObjectId"
+        },
+        userId: {
+          bsonType: "objectId",
+          description: "must be a valid user ObjectId"
+        },
+        rentDate: {
+          bsonType: "date",
+          description: "must be a date"
+        },
+        returnDate: {
+          bsonType: "date",
+          description: "must be a date"
+        }
+      }
+    }
+  }
+})
